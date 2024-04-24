469c5fd6ceca7e0082a02fe09f16b3e294e49f00
yagoomati6556 <166487898+yagoomati6556@users.noreply.github.com>
2024-04-24 20:17:23 +0200

    Delete java.js

--- java.js
+++
@@ -1,8 +0,0 @@
-var profesion ="encargado de la revista";
-var nombre ="Yagomati";
-
-var concatenacion = profesion + " " + nombre;
-
-var dat = document.getElementById("dat");
-datos.innerHTML = concatenacion;
-
