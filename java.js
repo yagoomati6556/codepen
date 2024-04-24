764fab749e0f087e9f97fb9a8e6524666fd4e0cc
yagoomati6556 <166487898+yagoomati6556@users.noreply.github.com>
2024-04-24 20:15:23 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+var profesion ="encargado de la revista";
+var nombre ="Yagomati";
+
+var concatenacion = profesion + " " + nombre;
+
+var datos = document.getElementById("datos");
+datos.innerHTML = concatenacion;
+
